cfa08fc62d6f68155ba66ab753d30f8b221d23e5
rakaarfi <rakaarfi@gmail.com>
2024-12-23 13:23:37 +0700

    Adjusting with search and api handling

---
+++ app/data-bahan-kimia/page.jsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import DataBahanKimiaTable from "@/components/DataBahanKimiaTable";
+import Pagination from "@/components/Pagination";
+import { Navbar } from "@/components/Navbar";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { SearchQuery } from "@/components/SearchQuery";
+import { fetchData, handleDelete, handleUpdate } from "@/components/HandleAPI";
+
+
+export default function page() {
+    const searchParams = useSearchParams();
+
+    // Ambil parameter dari URL
+    const queryPage = searchParams.get("page") || "1"; // Default ke 1
+    const querySearch = searchParams.get("search") || ""; // Default ke ''
+
+    const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(Number(queryPage))
+    const [totalPages, setTotalPages] = useState(0);
+    const [search, setSearch] = useState(querySearch);
+    const [error, setError] = useState(null);
+
+    const apiUrl = "http://127.0.0.1:8000/";
+    const routeUrl = "data_bahan_kimia";
+    const responseKey = "list_data_bahan_kimia";
+
+    const [characteristics, setCharacteristics] = useState([]);
+    const [lokasiBahanKimia, setLokasiBahanKimia] = useState([]);
+    const [dataPabrikPembuat, setDataPabrikPembuat] = useState([]);
+
+    const fetchData = async () => {
+        try {
+            const url = `${apiUrl+routeUrl}`;
+            const response = await axios.get(
+                `${url}/${responseKey}?page=${currentPage}&search=${search}`
+            );
+
+            const {
+                data: listData,
+                page,
+                total_pages,
+            } = response.data.list_data_bahan_kimia;
+
+            const { characteristics, lokasi_bahan_kimia, data_pabrik_pembuat } = response.data;
+
+            setData(listData);
+            setCurrentPage(page);
+            setTotalPages(total_pages);
+
+            // Store additional data in state
+            setCharacteristics(characteristics);
+            setLokasiBahanKimia(lokasi_bahan_kimia.data);
+            setDataPabrikPembuat(data_pabrik_pembuat.data);
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
+
+    // Sinkronisasi query parameter ke state
+    useEffect(() => {
+        setCurrentPage(Number(queryPage)); // Sinkronkan currentPage dengan query
+        setSearch(querySearch); // Sinkronkan search dengan query
+    }, [queryPage, querySearch]);
+
+    useEffect(() => {
+        fetchData();
+    }, [currentPage, search]);
+
+    return (
+        <>
+            <nav>
+                <Navbar />
+            </nav>
+            <div className="lg:mx-[20rem] mx-[9rem] lg:my-0 my-24">
+                <h1 className="text-2xl font-bold text-center mb-4 whitespace-nowrap">Daftar Data Bahan Kimia</h1>
+                <div className="flex flex-row justify-between items-center">
+                    <Link href="/lokasi-bahan-kimia/add" className="bg-blue-500 text-white px-4 py-2 rounded mx-10 mt-4">
+                        Input Data Bahan Kimia
+                    </Link>
+                    <SearchQuery
+                        searchQuery={search}
+                        setSearchQuery={setSearch}
+                        placeHolder={"Cari Data Bahan Kimia"}
+                    />
+                </div>
+                <DataBahanKimiaTable
+                    data={data}
+                    characteristics={characteristics}
+                    lokasiBahanKimia={lokasiBahanKimia}
+                    dataPabrikPembuat={dataPabrikPembuat}
+                    error={error}
+                    onUpdate={handleUpdate}
+                    onDelete={handleDelete}
+                    apiUrl={apiUrl}
+                    routeUrl={routeUrl}
+                />
+                <Pagination
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onPageChange={setCurrentPage}
+                />
+            </div>
+        </>
+    );
+}
--- components/DataBahanKimia.jsx
+++ components/DataBahanKimia.jsx
@@ -1,58 +1,176 @@
 'use client';
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function DataBahanKimia() {
+    const [locations, setLocations] = useState([]);
+    const [factories, setFactories] = useState([]);
+    const [name, setName] = useState("");
+    const [trade_name, setTradeName] = useState("");
+    const [chemical_formula, setChemicalFormula] = useState("");
+    const [id_factory, setIdFactory] = useState("");
+    const [id_location, setIdLocation] = useState("");
+    const [characteristic, setCharacteristic] = useState("");
+    const [max_amount, setMaxAmount] = useState("");
+    const [unit, setUnit] = useState("");
+    const [description, setDescription] = useState("");
+
+
+    const apiUrl = "http://127.0.0.1:8000";
+
+    // Fetch data for locations and factories
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const locationResponse = await axios.get(`${apiUrl}/lokasi_bahan_kimia/read`);
+                const factoryResponse = await axios.get(`${apiUrl}/data_pabrik_pembuat/read`);
+                setLocations(locationResponse.data);
+                setFactories(factoryResponse.data);
+            } catch (error) {
+                console.error("Error fetching data:", error.message);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Form submitted");
+
+        const payload = {
+            name,
+            trade_name,
+            chemical_formula,
+            id_factory: parseInt(id_factory),
+            id_location: parseInt(id_location),
+            characteristic,
+            max_amount: parseFloat(max_amount),
+            unit,
+            description,
+        };
+
+        try {
+            const response = await axios.post(`${apiUrl}/data_bahan_kimia/create/`, payload);
+            alert("Data berhasil disimpan!");
+        } catch (error) {
+            console.error("Error submitting data:", error.message);
+            alert("Terjadi kesalahan saat menyimpan data.");
+        }
+    };
 
     return (
         <div className="flex justify-center items-center min-h-screen">
             <div className="justify-items-center border rounded-3xl p-4 shadow-lg">
                 <h1 className="text-2xl font-bold mb-4 text-center">Data Bahan Kimia</h1>
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <table className="table-auto border-collapse">
                         <tbody>
                             <tr>
                                 <td className="px-4 py-2">Ruang:</td>
                                 <td className="px-4 py-2">
-                                    <input type="text" id="room" name="room" className="ruang-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6" required />
+                                    <select
+                                        id="room"
+                                        name="id_location"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5"
+                                        required
+                                        value={id_location}
+                                        onChange={(e) => setIdLocation(e.target.value)}
+                                    >
+                                        <option disabled defaultValue={""}>
+                                            Pilih Ruang
+                                        </option>
+                                        {locations.map((location) => (
+                                            <option key={location.id} value={location.id}>
+                                                {location.room}
+                                            </option>
+                                        ))}
+                                    </select>
                                 </td>
                             </tr>
                             <tr>
                                 <td className="px-4 py-2">Nama Bahan Kimia:</td>
                                 <td className="px-4 py-2">
-                                    <input type="text" id="name" name="name" className="lokasi-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6" required />
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        name="name"
+                                        className="lokasi-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
+                                        required
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
                                 </td>
                             </tr>
                             <tr>
                                 <td className="px-4 py-2">Nama Dagang:</td>
                                 <td className="px-4 py-2">
-                                    <input type="text" id="trade_name" name="trade_name" className="bangunan-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6" required />
+                                    <input
+                                        type="text"
+                                        id="trade_name"
+                                        name="trade_name"
+                                        className="bangunan-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
+                                        required
+                                        value={trade_name}
+                                        onChange={(e) => setTradeName(e.target.value)}
+                                    />
                                 </td>
                             </tr>
                             <tr>
                                 <td className="px-4 py-2">Rumus Kimia:</td>
                                 <td className="px-4 py-2">
-                                    <input type="text" id="chemical_formula" name="chemical_formula" className="bangunan-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6" required />
+                                    <input
+                                        type="text"
+                                        id="chemical_formula"
+                                        name="chemical_formula"
+                                        className="bangunan-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
+                                        required
+                                        value={chemical_formula}
+                                        onChange={(e) => setChemicalFormula(e.target.value)}
+                                    />
                                 </td>
                             </tr>
                             <tr>
                                 <td className="px-4 py-2">Pabrik Pembuat:</td>
                                 <td className="px-4 py-2">
-                                    <input type="text" id="factory_name" name="factory_name" className="contact-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6" required />
+                                    <select
+                                        id="factory_name"
+                                        name="id_factory"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5"
+                                        required
+                                        value={id_factory}
+                                        onChange={(e) => setIdFactory(e.target.value)}
+                                    >
+                                        <option disabled defaultValue={""}>
+                                            Pilih Pabrik
+                                        </option>
+                                        {factories.map((factory) => (
+                                            <option key={factory.id} value={factory.id}>
+                                                {factory.name}
+                                            </option>
+                                        ))}
+                                    </select>
                                 </td>
                             </tr>
                             <tr>
                                 <td className="px-4 py-2">Karakteristik:</td>
                                 <td className="px-4 py-2">
-                                    <select id="characteristic" name="characteristic" className="karakteristik-bahan p-2.5 block w-full rounded-md bg-white text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6" required >
-                                        <option value="" disabled selected>Pilih Karakteristik</option>
-                                        <option value={"Flammable"}>Flammable</option>
-                                        <option value={"Toxic"}>Toxic</option>
-                                        <option value={"Corrosive"}>Corrosive</option>
-                                        <option value={"Explosive"}>Explosive</option>
-                                        <option value={"Carcinogen"}>Carcinogen</option>
-                                        <option value={"Iritating"}>Iritating</option>
+                                    <select
+                                        id="characteristic"
+                                        name="characteristic"
+                                        className="karakteristik-bahan p-2.5 block w-full rounded-md bg-white text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
+                                        required
+                                        value={characteristic}
+                                        onChange={(e) => setCharacteristic(e.target.value)}>
+                                        <option disabled defaultValue={""}>
+                                            Pilih Karakteristik
+                                        </option>
+                                        <option value="Flammable">Flammable</option>
+                                        <option value="Toxic">Toxic</option>
+                                        <option value="Corrosive">Corrosive</option>
+                                        <option value="Explosive">Explosive</option>
+                                        <option value="Carcinogen">Carcinogen</option>
+                                        <option value="Iritating">Iritating</option>
                                     </select>
                                 </td>
                             </tr>
@@ -60,15 +178,41 @@ export default function DataBahanKimia() {
                                 <td className="px-4 py-2">Jumlah Inventori Maksimal:</td>
                                 <td className="px-4 py-2">
                                     <div className="flex flex-row gap-1">
-                                        <input type="text" id="max_amount" name="max_amount" className="jumlah-max-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6" required />
-                                        <input type="text" id="unit" name="unit" className="satuan-max-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6" placeholder="Satuan" required />
+                                        <input
+                                            type="number"
+                                            step="any"
+                                            id="max_amount"
+                                            name="max_amount"
+                                            className="jumlah-max-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
+                                            required
+                                            value={max_amount}
+                                            onChange={(e) => setMaxAmount(e.target.value)}
+                                        />
+                                        <input
+                                            type="text"
+                                            id="unit"
+                                            name="unit"
+                                            className="satuan-max-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
+                                            placeholder="Satuan"
+                                            required
+                                            value={unit}
+                                            onChange={(e) => setUnit(e.target.value)}
+                                        />
                                     </div>
                                 </td>
                             </tr>
                             <tr>
                                 <td className="px-4 py-2">Keterangan:</td>
                                 <td className="px-4 py-2">
-                                    <textarea name="description" id="description" rows="2" className="informasi-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6" required ></textarea>
+                                    <textarea
+                                        name="description"
+                                        id="description"
+                                        rows="2"
+                                        className="informasi-bahan block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
+                                        required
+                                        value={description}
+                                        onChange={(e) => setDescription(e.target.value)}
+                                    ></textarea>
                                 </td>
                             </tr>
                             <tr>

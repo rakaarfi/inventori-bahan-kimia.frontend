71683466123af0fc725bf926ed49f53bd9ae68c1
rakaarfi <rakaarfi@gmail.com>
2024-12-27 22:09:20 +0700

    add new directory for new report

---
+++ app/dashboard/report-data-bahan-kimia/page.jsx
@@ -0,0 +1,13 @@
+import ReportDataBahanKimia from '@/components/ReportDataBahanKimia'
+import React from 'react'
+
+export default function page() {
+    return (
+        <>
+            <div className="lg:ml-[20rem] lg:mr-[14rem] ml-[9rem] lg:my-0 my-24">
+                <ReportDataBahanKimia
+                />
+            </div>
+        </>
+    )
+}

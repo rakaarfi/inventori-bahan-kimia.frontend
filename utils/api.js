8eb208ab989fb159d822ea2bbd1bd6992a0a0dca
rakaarfi <rakaarfi@gmail.com>
2024-12-27 22:10:00 +0700

    move update and delete handling to api.js

--- utils/api.js
+++ utils/api.js
@@ -58,6 +58,34 @@ export const readById = async ({ routeUrl, id }) => {
 };
 
 // Update
+export const handleUpdate = async (e, id, routeUrl) => {
+    e.preventDefault();
 
+    const formData = new FormData(e.target);
+    const updatedData = Object.fromEntries(formData.entries());
+
+    try {
+        const response = await axios.post(
+            `${apiUrl}${routeUrl}/update/${id}`,
+            updatedData
+        );
+        alert("Data updated successfully!");
+
+    } catch (err) {
+        alert("Error updating data: " + err.message);
+    }
+};
 
 // Delete
+export const handleDelete = async (id, routeUrl) => {
+    const isConfirmed = window.confirm("Are you sure you want to delete this data?");
+    if (isConfirmed) {
+    try {
+        await axios.post(`${apiUrl}${routeUrl}/delete/${id}`);
+        alert("Data deleted successfully!");
+
+    } catch (err) {
+        alert("Error deleting data: " + err.message);
+    }
+}
+};
